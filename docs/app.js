c9fb6ed0e1138a4793dd639f35dee992602af8aa
Diana <dk3934@nyu.edu>
2025-12-18 20:02:10 -0500

    fix filepath

--- docs/app.js
+++ docs/app.js
@@ -4,7 +4,7 @@ marked.setOptions({ gfm:true, mangle:false, headerIds:false });
 
 const ORDERED_NOTES = [
   { file: "ordinary_least_squares.md", title: "Section 1: Ordinary Least Squares" },
-  { file: "omitted variables.md", title: "Section [?]: Much Ado About Omitted Variables" },
+  { file: "omitted_variables.md", title: "Section [?]: Much Ado About Omitted Variables" },
 ];
 
 function $id(id){ return document.getElementById(id); }
